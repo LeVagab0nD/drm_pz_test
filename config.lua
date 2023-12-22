8a72044e4892e122cede43babaa5aab55f1ed1b9
Vagabond <63746545+LeVagab0nD@users.noreply.github.com>
2023-12-22 11:10:36 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+-- config.lua
+local Config = {}
+
+Config.APIKey = "APIkey"
+Config.ModID = "WorkshopModID"  -- Remplacez par l'ID réel de votre mod
+Config.PHPURL = "https://example.com/your-php-script.php"  -- Ajoutez l'URL PHP ici
+
+return Config
